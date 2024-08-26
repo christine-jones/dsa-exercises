469c75ef9cdda05d0b1f7f5a6973eb2681575479
christine-jones <158510402+christine-jones@users.noreply.github.com>
2024-08-26 09:17:06 -0700

    document the Percolation class

--- Algorithms-Part1/Percolation/Percolation.h
+++ Algorithms-Part1/Percolation/Percolation.h
@@ -1,7 +1,7 @@
 /**
  * \file    Percolation.h
  * \author  Christine Jones 
- * \brief
+ * \brief   
  *
  * \copyright 2024
  * \license   GNU GENERAL PUBLIC LICENSE version 3 
@@ -16,11 +16,28 @@
 #include <iostream>
 #include <random>
 
+/**
+ * Class that models a percolation system.
+ * 
+ * Creates an n-by-n grid, with all sites intially blocked. A method is
+ * provided to open a given grid site and connect that newly open grid site
+ * to any open neighboring grid sites. The system percolates when a grid site
+ * in the bottom row is connected to a grid site in the top row via a chain
+ * of open neighboring grid sites.
+ * 
+ * An open-enabled UnionFind algorithm is required as class input via template
+ * parameter. The UnionFind algorithm manages connections within the grid.
+ */
 template <typename UF>
 class Percolation {
 
 public:
 
+    /**
+     * Constructor. Initializes a completely blocked n-by-n grid
+     * 
+     * \param int Grid size; must be greater than zero.
+     */
     explicit Percolation(int n):
         m_openUF{(n * n) + 2}, // 2 additional sites for virtual top and bottom
         m_grid_size{n},
@@ -30,21 +47,37 @@ public:
     {
         assert(n > 0);
 
-        // open virtual top and bottom for connections
+        // open virtual top and bottom
         m_openUF.open(m_top_index);
         m_openUF.open(m_bottom_index);
     }
 
+    /**
+     * Determine if the given grid site is open.
+     * 
+     * \param int Row index; must be greater than zero and less than or equal
+     *            to grid size.
+     * \param int Column index; must be greater than zero and less than or
+     *            equal to grid size.
+     * 
+     * \return bool True if grid site is open; False otherwise.
+     */
     bool isOpen(int row, int col) const {
-
-        assert(isValidGridIndex(row, col));
         
         return m_openUF.isOpen(indexIntoUF(row, col));
     }
-
+    /**
+     * Determine if the given grid site is full, i.e., connected to top row of
+     * grid via a chain of open neighboring grid sites.
+     * 
+     * \param int Row index; must be greater than zero and less than or equal
+     *            to grid size.
+     * \param int Column index; must be greater than zero and less than or
+     *            equal to grid size.
+     * 
+     * \return bool True if grid site is full; False otherwise.
+     */
     bool isFull(int row, int col) {
-
-        // validity of (row, col) checked by isOpen() call
 
         if (!isOpen(row, col))
             return false;
@@ -52,11 +85,27 @@ public:
         return m_openUF.connected(m_top_index, indexIntoUF(row, col));
     }
 
+    /**
+     * Return the total number of open sites within the grid.
+     * 
+     * \param int Row index; must be greater than zero and less than or equal
+     *            to grid size.
+     * \param int Column index; must be greater than zero and less than or
+     *            equal to grid size.
+     * 
+     * \return int Total number of open sites within the grid.
+     */
     int numberOfOpenSites() const { return m_num_open_sites; }
 
+    /**
+     * Open the given grid site and connect to any open neighbors.
+     * 
+     * \param int Row index; must be greater than zero and less than or equal
+     *            to grid size.
+     * \param int Column index; must be greater than zero and less than or
+     *            equal to grid size.
+     */
     void open(int row, int col) {
-
-        // validity of (row, col) checked by isOpen() call
 
         if (isOpen(row, col))
             return;
@@ -67,11 +116,22 @@ public:
         connectNeighbors(row, col);
     }
 
+    /**
+     * Determine if the system percolates, i.e., a site at the bottom of the
+     * grid is open and connected to a site at the top of the grid via a chain
+     * of open neighboring grid sites.
+     * 
+     * \return bool True if the system percolates; False otherwise.
+     */
     bool percolates() {
 
         return m_openUF.connected(m_top_index, m_bottom_index);
     }
 
+    /**
+     * Print n-by-n grid and additional class data to stdout for testing
+     * purposes.
+     */
     void print() const {
 
         std::cout << "   TOP: " << m_openUF.getID(0) << '\n'
@@ -92,17 +152,27 @@ public:
 
 private:
 
+    /**
+     * Verify that the row/column grid index is valid.
+     */
     bool isValidGridIndex(int i, int j) const {
         
         return (i > 0) && (i <= m_grid_size) &&
                (j > 0) && (j <= m_grid_size);
     }
 
+    /**
+     * Convert row/column grid index into vector index for UnionFind algorithm.
+     */
     int indexIntoUF(int row, int col) const {
 
+        assert(isValidGridIndex(row, col));
         return ((row - 1) * m_grid_size) + col;
     }
 
+    /**
+     * Connect the given grid site to any open grid neighbors.
+     */
     void connectNeighbors(int row, int col) {
 
         connectTopNeighbor(row, col);
@@ -138,16 +208,6 @@ private:
     void connectRightNeighbor(int row, int col) {
 
         // there is no right neighbor
-        if (col == 1)
-            return;
-
-        if (isOpen(row, col - 1))
-            m_openUF.join(indexIntoUF(row, col - 1), indexIntoUF(row, col));
-    }
-
-    void connectLeftNeighbor(int row, int col) {
-
-        // there is no left neighbor
         if (col == m_grid_size)
             return;
 
@@ -155,9 +215,30 @@ private:
             m_openUF.join(indexIntoUF(row, col + 1), indexIntoUF(row, col));
     }
 
+    void connectLeftNeighbor(int row, int col) {
+
+        // there is no left neighbor
+        if (col == 1)
+            return;
+
+        if (isOpen(row, col - 1))
+            m_openUF.join(indexIntoUF(row, col - 1), indexIntoUF(row, col));
+    }
+
+    // UnionFind algorithm used to manage open connections within the grid.
+    //
+    // The n-by-n grid is stored as a flat vector. In addition there is a
+    // "top" and "bottom" included. Therefore, the size of the UnionFind vector
+    // is ((n*n) + 2).
+    //
+    // The top row of the grid connects the "top" and bottom row of the grid to
+    // the "bottom". The system percolates when the virtual "top" and "bottom"
+    // sites are connected via a path of open grid sites.
     OpenUF<UF>  m_openUF;
 
     const int m_grid_size{};
+
+    // virtual top and bottom; system percolates when top and bottom connect
     const int m_top_index{};
     const int m_bottom_index{};
 
