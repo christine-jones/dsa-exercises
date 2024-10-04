9159d5c9e9191ab7266e3ea38ccfd0d1d109676a
christine-jones <158510402+christine-jones@users.noreply.github.com>
2024-10-04 07:06:44 -0700

    implemented client program as specified given assignment

--- Algorithms-Part1/Queues/src/main.cpp
+++ Algorithms-Part1/Queues/src/main.cpp
@@ -7,19 +7,68 @@
  * \license   GNU GENERAL PUBLIC LICENSE version 3 
  */
 
-#include "Deque.h"
 #include "RandomQueue.h"
-#include "Test.h"
 #include <iostream>
+#include <sstream>
+#include <string>
+
+void printUsage() {
+
+    std::cout << "Usage: <program name> <k>" << '\n';
+    std::cout << "\tk = number of strings to print to standard output,\n"
+              << "\t    where 0 <= k <= n, and n is the number of strings\n"
+              << "\t    read from standard input\n";
+}
 
 /**
  * Main program.
  * 
- * Usage: <program name>
+ * Usage: <program name> <k>
+ *      k = number of strings to print to standard output, where 0 <= k <= n,
+ *          and n is the number of strings read from standard input
  * 
  */
-int main() {
+int main(int argc, char* argv[]) {
 
-    Test::runTests();
+    if (argc != 2) {
+
+        printUsage();
+        return 1;        
+    }
+
+    int k{};
+    std::stringstream ssarg1{argv[1]};
+
+    if (!(ssarg1 >> k)) {
+
+        printUsage();
+        return 1;
+    }
+
+    RandomQueue<std::string> random_words{};
+
+    // read entire line of words
+    std::string words{};
+    std::getline(std::cin, words);
+
+    // handle each word individually
+    std::istringstream iss{words};
+    std::string word{};
+    while (iss >> word) {
+
+        random_words.enqueue(word);
+    }
+
+    if (k < 0 || k > random_words.size()) {
+
+        std::cout << "The given value of k is invalid: " << k << '\n';
+        return 0;
+    }
+
+    RandomQueue<std::string>::const_iterator iter{random_words.begin()};
+    for (int i{0}; i < k; ++i, ++iter) {
+        std::cout << *iter << '\n';
+    }
+
     return 0;
 }
