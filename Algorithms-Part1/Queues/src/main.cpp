0cfe5127bc59c19fb46b1b86ee29260c4a45369c
christine-jones <158510402+christine-jones@users.noreply.github.com>
2024-10-07 09:59:12 -0700

    updated client program to store only k random words

--- Algorithms-Part1/Queues/src/main.cpp
+++ Algorithms-Part1/Queues/src/main.cpp
@@ -7,6 +7,7 @@
  * \license   GNU GENERAL PUBLIC LICENSE version 3 
  */
 
+#include "Random.h"
 #include "RandomQueue.h"
 #include <iostream>
 #include <sstream>
@@ -45,7 +46,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    if (k < 0) {
+
+        std::cout << "Given value of k is invalid, must be >= 0: "
+                  << k << '\n';
+        printUsage();
+        return 1;
+    }
+
     RandomQueue<std::string> random_words{};
+
+    // number of words read thus far
+    int num_words{0};
 
     // read entire line of words
     std::string words{};
@@ -56,16 +68,29 @@ int main(int argc, char* argv[]) {
     std::string word{};
     while (iss >> word) {
 
-        random_words.enqueue(word);
+        if (num_words < k)
+
+            random_words.enqueue(word);
+        
+        else if (Random::getRandomNumber(0, num_words) < k) {
+
+            random_words.sample() = word;
+
+        }
+
+        ++num_words;        
     }
 
-    if (k < 0 || k > random_words.size()) {
+    if (k > random_words.size()) {
 
-        std::cout << "The given value of k is invalid: " << k << '\n';
+        std::cout << "The given value of k is invalid, must be <= than the "
+                  << "number of given words: " << k << '\n';
         return 0;
     }
+    
+    assert(random_words.size() == k);
 
-    RandomQueue<std::string>::const_iterator iter{random_words.begin()};
+    RandomQueue<std::string>::const_iterator iter{random_words.cbegin()};
     for (int i{0}; i < k; ++i, ++iter) {
         std::cout << *iter << '\n';
     }
