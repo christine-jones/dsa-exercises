16d3ea628af1c8e44377032fb3f0a88a8debf51f
christine-jones <158510402+christine-jones@users.noreply.github.com>
2024-10-28 08:26:20 -0700

    read game board from input file

--- Algorithms-Part1/SliderPuzzle/src/main.cpp
+++ Algorithms-Part1/SliderPuzzle/src/main.cpp
@@ -9,19 +9,81 @@
 
 #include "Board.h"
 #include "BoardSolver.h"
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main() {
+void printUsage() {
 
-    Board b1{board{{1, 2, 3},
-                   {0, 7, 6},
-                   {5, 4, 8}}};
-    BoardSolver solver{b1};
+    std::cout << "Usage: <program name> <input-file>" << '\n';
+    std::cout << "\tinput_file = file that contains input data" << '\n';
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        printUsage();
+        return 1;        
+    }
+
+    std::ifstream input_file{argv[1]};
+    if (!input_file) {
+        std::cerr << "Failed to open input file." << '\n';
+        return 1;
+    }
+
+    std::string first_line{};
+    if (!std::getline(input_file, first_line)) {
+        std::cerr << "Failed to read first line from input file." << '\n';
+        return 1;
+    }
+
+    std::istringstream first_iss{first_line};
+    int grid_size{};
+    if (!(first_iss >> grid_size)) {
+        std::cerr << "Failed to read number of points from input file." << '\n';
+        return 1;
+    }
+
+    if (grid_size < 2 || grid_size >= 128) {
+        std::cerr << "Invalid grid size: " << grid_size << '\n';
+        return 1;
+    }
+
+    board binput{board(static_cast<std::size_t>(grid_size),
+                       std::vector<int>(static_cast<std::size_t>(grid_size)))};
+
+    for (std::size_t i{0}; i < static_cast<std::size_t>(grid_size); ++i) {
+
+        std::string line{};
+        if (!std::getline(input_file, line)) {
+            std::cerr << "Failed to read line from input file." << '\n';
+            return 1;
+        }
+
+        std::istringstream iss{line};
+        for (std::size_t j{0}; j < static_cast<std::size_t>(grid_size); ++j) {
+            if (!(iss >> binput[i][j])) {
+                std::cerr << "Failed to read data from input file." << '\n';
+                return 1;
+            }
+        }
+    }
+
+    input_file.close();
+
+    Board b{binput};
+    if (!b.isValid()) {
+        std::cerr << "Invalid board game." << '\n';
+        return 1;
+    }
+
+    BoardSolver solver{b};
 
     std::cout << "SOLVED = " << (solver.isSolvable() ? "true" : "false") << " "
               << "MOVES = " << solver.moves() << '\n';
-    for (auto& b : solver.solution())
-        std::cout << b << '\n';
+    for (auto& move : solver.solution())
+        std::cout << move << '\n';
 
     return 0;
 }
