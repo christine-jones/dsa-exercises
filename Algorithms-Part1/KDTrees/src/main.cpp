ac3457f434cda3b383ae1e10db9a188ac4506a44
christine-jones <158510402+christine-jones@users.noreply.github.com>
2024-11-15 07:54:08 -0800

    main client that reads input test data

--- Algorithms-Part1/KDTrees/src/main.cpp
+++ Algorithms-Part1/KDTrees/src/main.cpp
@@ -9,55 +9,64 @@
 
 #include "KDTree.h"
 #include "Point2D.h"
+#include "PointSet.h"
 #include "Rectangle.h"
+#include "StopWatch.h"
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main() {
+void printUsage() {
+
+    std::cout << "Usage: <program name> <input-file>" << '\n';
+    std::cout << "\tinput_file = file that contains list of points"
+              << '\n';
+}
+
+/**
+ * Client program.
+ * 
+ * Usage: <program name> <input_file>
+ *      input_file = file that contains a list of points
+ */
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        printUsage();
+        return 1;        
+    }
+
+    std::ifstream input_file{argv[1]};
+    if (!input_file) {
+        std::cerr << "Failed to open input file." << '\n';
+        return 1;
+    }
 
     KDTree tree{};
 
-    tree.insert(Point2D{0.7, 0.2});
-    tree.insert(Point2D{0.5, 0.4});
-    tree.insert(Point2D{0.2, 0.3});
-    tree.insert(Point2D{0.4, 0.7});
-    tree.insert(Point2D{0.9, 0.6});
+    std::string line{};
+    while (std::getline(input_file, line)) {
 
-    std::cout << tree << '\n';
+        std::istringstream iss{line};
+        double x{}, y{};
+        if (!(iss >> x >> y)) {
+            std::cerr << "Failed to read point data from input file." << '\n';
+            return 1;
+        }
 
-    std::cout << "Contains " << Point2D{0.2, 0.3} << ": "
-              << (tree.contains(Point2D{0.2, 0.3}) ? "TRUE" : "FALSE")
-              << '\n';
-    std::cout << "Contains " << Point2D{0.2, 0.4} << ": "
-              << (tree.contains(Point2D{0.2, 0.4}) ? "TRUE" : "FALSE")
-              << '\n';
+        tree.insert(Point2D{x, y});
+    }
 
-    std::cout << "Range " << Rectangle::unitSquareRectangle() << ": ";
-    for(const auto& p : tree.range(Rectangle::unitSquareRectangle()))
-        std::cout << p << " ";
-    std::cout << '\n';
-    std::cout << "Range " << Rectangle{0.3, 0.1, 0.4, 0.3} << ": ";
-    for(const auto& p : tree.range(Rectangle{0.3, 0.1, 0.4, 0.3}))
-        std::cout << p << " ";
-    std::cout << '\n';
-    std::cout << "Range " << Rectangle{0.3, 0.1, 0.8, 0.8} << ": ";
-    for(const auto& p : tree.range(Rectangle{0.3, 0.1, 0.8, 0.8}))
-        std::cout << p << " ";
-    std::cout << '\n';
-    std::cout << "Range " << Rectangle{0.1, 0.5, 0.6, 0.8} << ": ";
-    for(const auto& p : tree.range(Rectangle{0.1, 0.5, 0.6, 0.8}))
-        std::cout << p << " ";
-    std::cout << '\n';
+    input_file.close();
 
-    std::cout << "Nearest " << Point2D{0.1, 0.9} << ": "
-              << tree.nearest(Point2D{0.1, 0.9}) << '\n';
-    std::cout << "Nearest " << Point2D{0.9, 0.1} << ": "
-              << tree.nearest(Point2D{0.9, 0.1}) << '\n';
-    std::cout << "Nearest " << Point2D{0.9, 0.9} << ": "
-              << tree.nearest(Point2D{0.9, 0.9}) << '\n';
-    std::cout << "Nearest " << Point2D{0.1, 0.1} << ": "
-              << tree.nearest(Point2D{0.1, 0.1}) << '\n';
-    std::cout << "Nearest " << Point2D{0.5, 0.4} << ": "
-              << tree.nearest(Point2D{0.5, 0.4}) << '\n';
+    std::cout << "Number of Nodes = " << tree.size() << '\n';
+
+    StopWatch timer{};
+    Point2D nearest{tree.nearest(Point2D{0.81, 0.30})};
+    double elapsed{timer.elapsed()};
+
+    std::cout << "Nearest to " << Point2D{0.81, 0.30} << ": "
+              << nearest << " (" << elapsed << ")" << '\n';
 
     return 0;
 }
