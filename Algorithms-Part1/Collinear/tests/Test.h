2d399a274b83833831cc2511626aa366d5dd8e74
christine-jones <158510402+christine-jones@users.noreply.github.com>
2024-12-04 08:33:04 -0800

    added testing infrastructure to projects

---
+++ Algorithms-Part1/Collinear/tests/Test.h
@@ -0,0 +1,24 @@
+/**
+ * \file    Test.h
+ * \author  Christine Jones 
+ * \brief   Test namespace; utilities to facilitate unit testing.
+ * 
+ * \copyright 2024
+ * \license   GNU GENERAL PUBLIC LICENSE version 3 
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <string_view>
+
+namespace Test {
+
+void reset();
+void runReport();
+
+void ASSERT(bool condition, std::string_view message);
+
+}
+
+#endif // TEST_H
---
+++ Algorithms-Part1/SliderPuzzle/tests/TestMain.cpp
@@ -0,0 +1,21 @@
+/**
+ * \file    TestMain.cpp
+ * \author  Christine Jones 
+ * \brief   Main program to run test cases for SliderPuzzle project.
+ * 
+ * \copyright 2024
+ * \license   GNU GENERAL PUBLIC LICENSE version 3 
+ */
+
+#include "Test.h"
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Running Tests..." << '\n' << '\n';
+    std::cout << "work in progress..." << '\n';
+    std::cout << '\n' << "COMPLETE" << '\n';
+
+    return 0;
+}
